b8c32600c519a5fc1ee3b3b14aac74a3b3e13085
roy-ayush22 <ayush30085@gmail.com>
2025-12-06 00:09:30 +0530

    database implementation

---
+++ server/schema.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  name: String,
+  email: String,
+  message: String,
+  date: { type: Date, default: Date.now },
+});
+
+const UserModel = mongoose.model("users", User);
+
+module.exports = UserModel;
